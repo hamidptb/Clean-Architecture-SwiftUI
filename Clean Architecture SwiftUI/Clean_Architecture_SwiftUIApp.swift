9379869fc3c8d2f354d4bd36f930b800a87b7e82
Hamid Reza Ansari <hamid.22.ansari@gmail.com>
2024-12-13 18:46:44 +0330

    Initial Commit

---
+++ Clean Architecture SwiftUI/Clean_Architecture_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clean_Architecture_SwiftUIApp.swift
+//  Clean Architecture SwiftUI
+//
+//  Created by Hamid on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Clean_Architecture_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
